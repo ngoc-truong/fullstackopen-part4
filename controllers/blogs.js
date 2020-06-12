0cbc3b3420ec4fd093b5a37b4109d800a2149c8c
Ngoc Truong <ngoc.truong@posteo.de>
2020-06-12 17:42:16 +0200

    Finish adding validation tokens

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require("express").Router();
+const jwt = require("jsonwebtoken");
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
@@ -24,6 +25,14 @@ blogsRouter.get("/:id", (request, response, next) => {
 		.catch(error => next(error));
 });
 
+const getTokenFrom = (request) => {
+	const authorization = request.get("authorization");
+	if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+		return authorization.substring(7);
+	}
+	return null;
+};
+
 // Post a new blog
 blogsRouter.post("/", async (request, response, next) => {
 	if (!request.body.likes){
@@ -31,7 +40,12 @@ blogsRouter.post("/", async (request, response, next) => {
 	}
 
 	const body = request.body; 
-	const user = await User.findById(body.userId);
+	const token = getTokenFrom(request);
+	const decodedToken = jwt.verify(token, process.env.SECRET);
+	if (!token || !decodedToken.id) {
+		return response.status(401).json({ error: "token missing or invalid" });
+	}
+	const user = await User.findById(decodedToken.id);
 
 	const blog = new Blog({
 		title: body.title,
